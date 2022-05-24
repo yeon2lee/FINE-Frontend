dec589ecc18ab8bf8c3af3f884ea4063e4c462c5
임현승 <ardimento22@naver.com>
2022-05-25 04:22:51 +0900

    test_commit

--- app/src/main/java/com/fine_app/MainActivity.kt
+++ app/src/main/java/com/fine_app/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
+
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
