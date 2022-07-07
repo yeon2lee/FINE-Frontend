06e1498156380d3d4a6bca38e979b55698b87723
yeon2lee <md04254@naver.com>
2022-07-07 13:54:43 +0900

    [FIX] bottom navigation connect error

--- app/src/main/java/com/fine_app/ui/myPage/MyPageFragment.kt
+++ app/src/main/java/com/fine_app/ui/myPage/MyPageFragment.kt
@@ -1,16 +1,22 @@
-package com.fine_app.ui.myPage
+package com.fine_app.ui.MyPage
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.fine_app.databinding.FragmentMypageBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyPageFragment : Fragment() {
 
     private var _binding: FragmentMypageBinding? = null
+    var userId: Long = 0
+    lateinit var userData: Profile
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,9 +27,11 @@ class MyPageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // 프로필 생성
+        getMyProfile()
 
         binding.tvManagePost.setOnClickListener {
             activity?.let{
@@ -32,13 +40,21 @@ class MyPageFragment : Fragment() {
             }
         }
 
-        binding.btnKeyword.setOnClickListener {
+        binding.box.setOnClickListener {
             activity?.let{
                 val intent = Intent(context, UpdateProfileActivity::class.java)
+                intent.putExtra("nickname", binding.tvNickname.text)
+                intent.putExtra("intro", binding.tvIntro.text)
                 startActivity(intent)
             }
         }
+
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getMyProfile()
     }
 
     override fun onDestroyView() {
@@ -46,5 +62,32 @@ class MyPageFragment : Fragment() {
         _binding = null
     }
 
+    private fun getMyProfile() {
+        userId = 1
+
+        val call: Call<Profile> = ServiceCreator.service.getMyProfile(userId)
+
+        call.enqueue(object : Callback<Profile> {
+            override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                if (response.isSuccessful) {
+                    userData = response.body()!!
+                    binding.tvNickname.setText(userData.nickname)
+                    binding.tvIntro.setText(userData.intro)
+
+                    Toast.makeText(context,"(MypageFragment) 성공", Toast.LENGTH_SHORT).show()
+                    val data = response.body()?.nickname
+                    Toast.makeText(context, "(MypageFragment) ${data}님 반갑습니다!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "(MypageFragment) 프로필 정보 불러오기 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Profile>, t: Throwable) {
+                Toast.makeText(context, "서버 연결 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+    }
 
 }
--- app/src/main/java/com/fine_app/ui/myPage/Post.kt
+++ app/src/main/java/com/fine_app/ui/myPage/Post.kt
@@ -1,3 +1,20 @@
-package com.fine_app.ui.myPage
+package com.fine_app.ui.MyPage
 
-data class Post(val title:String)
+data class Post(
+    val createdDate:String,
+    val lastModifiedDate:String,
+    val title:String,
+    val content: String,
+    val closingCheck:Boolean,
+    val group_check:Boolean,
+    val maxMember: Int,
+    val comments: ArrayList<Comment>,
+    val posting_id: Long,
+)
+
+
+data class Comment(
+    val nickname:String,
+    val text:String,
+    val profileID: Int
+)
